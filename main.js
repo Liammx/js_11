0c513e642170e8e60a65d2129085f47eb16c8c21
liammx <liammx03@gmial.com>
2023-10-28 14:40:23 +0500

    no baggs

---
+++ main.js
@@ -0,0 +1,92 @@
+// Отфильтровать задачи в переменные а и b
+    // Сохранить количество в ключе count
+    // Сохранить сами задачи в массиве-ключе arr
+    let arr = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut au`tem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+  
+            "completed": 
+true
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 6,
+            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 7,
+            "title": "illo expedita consequatur quia in",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 8,
+            "title": "quo adipisci enim quam ut ab",
+            "completed": true
+        },
+        {
+            "userId": 1,
+   
+            "id": 9,
+            "title": "molestiae perspiciatis ipsa",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 10,
+            "title": "illo est ratione doloremque quia maiores aut",
+            "completed": true
+        },
+    ]
+    
+let a = {
+    count: 0,
+    arr: []
+}
+
+let b = {
+    count: 0,
+    arr: []
+}
+
+for (let i = 0 ; i< arr.length; i++) {
+   if(arr[i].completed === true ){
+    a.count++
+    a.arr.push(arr[i])
+}
+    if(arr[i].completed === false ) {
+        b.count++
+        b.arr.push(arr[i])
+    }
+}
+
+console.log(a);
+console.log(b);
